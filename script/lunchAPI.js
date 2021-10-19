3750ea5a9ac84df609481924737e053759a4a8f1
bigmoondev1004 <bigmoondev1004@gmail.com>
2021-10-19 22:58:05 +0900

    호출 후 받은 데이터를 못다루고있음..

--- script/lunchAPI.js
+++ script/lunchAPI.js
@@ -1,17 +1,19 @@
-var APIrequest = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=f72d6690d0f54631beec677ec932846b&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010132&MMEAL_SC_CODE=2&MLSV_YMD=20211018";
+// var APIrequest = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=f72d6690d0f54631beec677ec932846b&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010132&MMEAL_SC_CODE=2&MLSV_YMD=20211018";
 
 $.ajax({
-    method: "POST",
+    method: "GET",
     url: "https://open.neis.go.kr/hub/mealServiceDietInfo",
-    data: {  KEY: "f72d6690d0f54631beec677ec932846b?",
+    data: {  
+    KEY: "f72d6690d0f54631beec677ec932846b",
     Type: "json",
     pindex : 1,
     pSize : 5,
     ATPT_OFCDC_SC_CODE : "B10",
     SD_SCHUL_CODE : 7010132,
     MMEAL_SC_CODE : 2,
-    MLSV_YMD : 20211018}
+    MLSV_YMD : 20211018},
 })
-    .done(function( msg ) {
-    alert( "Data Saved: " + msg );
-    });
+    .done(function(json) {
+    $("<h1>").text(json.mealServiceDietInfo[1].row[0].DDISH_NM).appendTo("body");
+    $("<div class=\"content\">").html(json.html).appendTo("body");
+});
