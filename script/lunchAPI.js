d47af4b379c5653cb34d5dbcb317b0ac3a35c4ee
bigmoondev1004 <bigmoondev1004@gmail.com>
2021-10-19 19:02:41 +0900

    jquery 적용 성공

--- script/lunchAPI.js
+++ script/lunchAPI.js
@@ -1,22 +1,17 @@
-$(document).reaady(function(){
-    var url = 'http://open.neis.go.kr/hub/schoolInfo?'
-            +'ATPT_OFCDC_C_CODE=T10&Type=json&pIndex=1&pSize=100';
-    var param = '';
-    ajaxCallApiTest(url, param, ApiCallBack);
+var APIrequest = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=f72d6690d0f54631beec677ec932846b&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010132&MMEAL_SC_CODE=2&MLSV_YMD=20211018";
+
+$.ajax({
+    method: "POST",
+    url: "https://open.neis.go.kr/hub/mealServiceDietInfo",
+    data: {  KEY: "f72d6690d0f54631beec677ec932846b?",
+    Type: "json",
+    pindex : 1,
+    pSize : 5,
+    ATPT_OFCDC_SC_CODE : "B10",
+    SD_SCHUL_CODE : 7010132,
+    MMEAL_SC_CODE : 2,
+    MLSV_YMD : 20211018}
+})
+    .done(function( msg ) {
+    alert( "Data Saved: " + msg );
     });
-    function ajaxCallApiTest(url, param, callback) {
-        $.ajax({
-        url: 'https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=f72d6690d0f54631beec677ec932846b&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010132&MMEAL_SC_CODE=2&MLSV_YMD=20211018',
-        async: true,
-        type: "POST",
-        data: param,
-        dataType: 'json',
-        success: callback,
-        error:function (request,teextStatus){
-            var format = new OpenLayers.Format.WFSDescribeFeatureType();
-            var doc = request.responseXML;
-            var describeFeatureType = format.read(doc);
-            alert(" describeFeatureType = "+ describeFeatureType + " textStatus = "+textStatus);
-        }
-    });
-}
